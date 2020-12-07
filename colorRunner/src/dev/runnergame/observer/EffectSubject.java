f6d347cea261dc0f67efa09b8ecbed08441217fb
Jonas Žalys <jonas.zalyz@gmail.com>
2020-12-07 21:52:49 +0200

    Fixed iterator

--- colorRunner/src/dev/runnergame/observer/EffectSubject.java
+++ colorRunner/src/dev/runnergame/observer/EffectSubject.java
@@ -1,5 +1,6 @@
 package dev.runnergame.observer;
 
+import dev.runnergame.entities.AccelerationPlatform;
 import dev.runnergame.entities.Effect;
 import dev.runnergame.iterator.PlatformObserverList;
 
@@ -7,6 +8,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class EffectSubject extends Effect {
     private PlatformObserverList observers = new PlatformObserverList();
@@ -19,10 +21,21 @@ public abstract class EffectSubject extends Effect {
         observers.removeObserver(observer);
     }
     public void notifyAllObservers(){
+       // ReentrantLock lock = new ReentrantLock();
+       // lock.lock();
+        System.out.println("Observer notified all");
+      /* for(Object obs : observers){
+            PlatformObserver observer = (PlatformObserver) obs;
+            observer.updateObserver();
+            System.out.println("Observer notified 1");
+        }*/
+      // lock.unlock();
+
         Iterator iter = observers.getIterator();
         while(iter.hasNext()) {
             PlatformObserver obs = (PlatformObserver) iter.next();
             obs.updateObserver();
+            System.out.println("Observer notified iter");
         }
     }
 }
